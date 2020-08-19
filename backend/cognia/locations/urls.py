147e8ce5bcfe65f13979bf78cb37d5679cb654c6
vinx-2105 <2017csb1119@iitrpr.ac.in>
2020-08-19 17:32:23 +0530

    created patient location history apis

--- backend/cognia/locations/urls.py
+++ backend/cognia/locations/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 # from .views import get_user_scores, register_game_score
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import get_user_location_history, register_user_location, get_user_last_location
+from .views import get_user_location_history, register_user_location, get_user_last_location, get_patients_last_location, get_patients_location_history
 
 urlpatterns = [
     path('get_user_location_history/', get_user_location_history, name='get_user_location_history'),
     path('get_user_last_location/', get_user_last_location, name='get_user_last_location'),
     path('register_user_location/', register_user_location, name='register_user_location'),
+    path('get_patients_location_history/', get_patients_location_history, name='get_patients_location_history'),
+    path('get_patients_last_location/', get_patients_last_location, name='get_patients_last_location')
 ]
